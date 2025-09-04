7cee9cba30983e4d7476b40a20826858b48b5bc8
Sarah Wooders <sarahwooders@gmail.com>
2025-09-03 21:16:42 -0700

    add search

--- src/python/tests/test_messages.py
+++ src/python/tests/test_messages.py
@@ -5,7 +5,7 @@ import time
 
 def test_conversational_memory():
     """ Test the conversational memory client """
-    client = Memory(letta_api_key=os.getenv("LETTA_API_KEY"))
+    client = Memory(api_key=os.getenv("LETTA_API_KEY"))
     test_user_id = "test_user_id_123"
 
     # initialize the userA
@@ -16,6 +16,14 @@ def test_conversational_memory():
         {
             "role": "user",
             "content": "Hi my name is Bob"
+        }, 
+        {
+            "role": "assistant",
+            "content": "Hi Bob, how can I help you today?"
+        },
+        {
+            "role": "user",
+            "content": "I love cats"
         }
     ])
 
@@ -26,5 +34,12 @@ def test_conversational_memory():
     memory = client.get_user_memory(test_user_id)
     assert "Bob" in memory, f"Expected 'Bob' in memory value, got {memory}"
     print(memory)
+
+    # search 
+    search_results = client.search(test_user_id, "animals")
+    assert len(search_results.results) > 0, f"Expected search results, got {search_results.results}"
+    print("SEARCH RESULTS", search_results.results)
+    assert "cats" in search_results.results[0].content, f"Expected 'cats' in search results, got {search_results.results[0].content}"
+    print(search_results)
 
 
